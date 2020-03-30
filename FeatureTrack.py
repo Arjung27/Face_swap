becf98ab94df9935cca6dde382fe44afd1f4c053
AbhishekB <abhibanerjee2494@gmail.com>
2020-03-30 18:48:27 -0400

    Kalman Update

--- FeatureTrack.py
+++ FeatureTrack.py
@@ -26,6 +26,9 @@ def videoDetector(fname,p):
 	frame_height = int(cap.get(4))
 	vidWriter = cv2.VideoWriter("./video_output.mp4",cv2.VideoWriter_fourcc(*'mp4v'), 24, (frame_width, frame_height))
 	i=0
+	sname = './TestSet_P2/Rambo.jpg'
+	img1 = cv2.imread(sname)
+	X = np.zeros((frame_height,frame_width,3))
 	#while(cap.isOpened()):
 	while(cap.isOpened()):
 		ret,frame = cap.read()
@@ -64,16 +67,29 @@ def videoDetector(fname,p):
 			ret, frame = cap.read()  # read another frame
 			if ret == False:
 				break
+			
 			prediction = kalman.predict()
-			#print(prediction.shape)
-			#print(prediction[0],prediction[1])
-			#print(prediction[2],prediction[3])
 			prediction = prediction.T
 			prediction = np.reshape(prediction,(136,2))
-			#print(prediction.shape)
+			
 			pred = prediction[:68,:]
 			#for (x,y) in pred:
-			#	cv2.circle(frame,(int(x),int(y)),2,(0,255,0),-1)
+				#cv2.circle(frame,(int(x),int(y)),2,(0,255,0),-1)
+			#------------------------------------)
+			if i==1:
+				F = frame
+			elif i==99 or i==106 or i==117 or i ==121 or i ==127 or i ==130 or i==136 or i==147 or i==201:
+				F = X
+			else:
+				pred = np.array(pred,dtype='int32')
+				_,V,VP,rectangle = triangulation(pred,frame)
+				_,shps = getFaceLandmarks(img1,p)
+				Vs,IMGs = doTriangulate(shps,VP,img1)
+				a,b = swapFace(frame,img1,V,Vs)
+				print(frame.shape)
+				A = interpolate(img1,frame,a,b)
+				F = blendFace(rectangle,frame,A)
+			#-------------------------------------
 			IMAGE,shp = getFaceLandmarks(frame,p)
 			shp = np.asarray(shp,dtype='float64')
 			st = shp.flatten().T
@@ -84,35 +100,15 @@ def videoDetector(fname,p):
 			if (shp[0,0]!=0.0):
 				posterior = kalman.correct(measurement)
 				final = posterior
-			#cv2.imshow('Out',frame)
-			#cv2.waitKey(0)
-			vidWriter.write(frame)
+			cv2.imshow('Out',F)
+			cv2.waitKey(0)
+			vidWriter.write(F)
+			X=F
 			i+=1
 			print(i)
 
 	cap.release()
 	vidWriter.release()
-
-
-def getFaceLandmarks(img,p):
-	detector = dlib.get_frontal_face_detector()
-	predictor = dlib.shape_predictor(p)
-	#img = cv2.imread(fname)
-	#img = imutils.resize(img,width = 320)
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	bbox = detector(gray,0)
-	if (len(bbox)==0):
-		s = np.zeros((68,2))
-		return img,s
-	
-	for (i,bbox) in enumerate(bbox):
-		shape = predictor(gray,bbox)
-		shape = face_utils.shape_to_np(shape)
-		
-		for (x,y) in shape:
-			cv2.circle(img,(x,y),2,(255,0,0),-1)
-
-	return img,shape
 
 def triangulation(land_points,img):
 	points = np.array(land_points, np.int32)
@@ -160,13 +156,129 @@ def triangulation(land_points,img):
 			vert.append((points[temp[0]],points[temp[1]],points[temp[2]]))
 			VPoint.append((temp[0],temp[1],temp[2]))
 		'''
-		cv2.line(img, tuple(points[temp[0]]), tuple(points[temp[1]]), (0, 0, 255), 2)
-		cv2.line(img, tuple(points[temp[1]]), tuple(points[temp[2]]), (0, 0, 255), 2)
-		cv2.line(img, tuple(points[temp[0]]), tuple(points[temp[2]]), (0, 0, 255), 2)
+		#cv2.line(img, tuple(points[temp[0]]), tuple(points[temp[1]]), (0, 0, 255), 2)
+		#cv2.line(img, tuple(points[temp[1]]), tuple(points[temp[2]]), (0, 0, 255), 2)
+		#cv2.line(img, tuple(points[temp[0]]), tuple(points[temp[2]]), (0, 0, 255), 2)
 	vert = np.asarray(vert)
 	VPoint = np.asarray(VPoint)
 	chull = np.reshape(chull,(chull.shape[0],chull.shape[2]))
 	return img,vert,VPoint,chull
+
+def doTriangulate(PIndex,TIndex,img):
+	T = []
+	for ti in TIndex:
+		T.append((PIndex[ti[0]],PIndex[ti[1]],PIndex[ti[2]]))
+	T = np.asarray(T)
+	#for t in T:
+	#	cv2.line(img, tuple(t[0]), tuple(t[1]), (0, 0, 255), 2)
+	#	cv2.line(img, tuple(t[1]), tuple(t[2]), (0, 0, 255), 2)
+	#	cv2.line(img, tuple(t[0]), tuple(t[2]), (0, 0, 255), 2)
+	return T, img
+
+def affineBary(img,ini_tri,fin_tri,size):
+	src = ini_tri
+	dst = fin_tri
+	x_min = min(src[0,0],src[1,0],src[2,0])
+	x_max = max(src[0,0],src[1,0],src[2,0])
+	y_min = min(src[0,1],src[1,1],src[2,1])
+	y_max = max(src[0,1],src[1,1],src[2,1])
+	x = np.linspace(x_min, x_max, x_max-x_min+1)
+	y = np.linspace(y_min, y_max, y_max-y_min+1)
+	mesh = np.meshgrid(x,y)
+	mesh = np.asarray(mesh)
+	mesh = mesh.reshape(*mesh.shape[:1], -1)
+	grid = np.vstack((mesh, np.ones((1, mesh.shape[1]))))
+	B = [[src[0][0],src[1][0],src[2][0]],[src[0][1],src[1][1],src[2][1]],[1,1,1]]
+	B_inv = np.linalg.inv(B)
+
+	bc = np.dot(B_inv,grid)
+
+	Z = []
+	D = []
+	for i in range(bc.shape[1]):
+		if bc[0,i]+bc[1,i]+bc[2,i]-1<0.0001 and 0<=bc[0,i] and 0<=bc[1,i] and 0<=bc[2,i] and bc[0,i]<=1 and bc[1,i]<=1 and bc[2,i]<=1:
+			Z.append(bc[:,i])
+			D.append((grid[0,i],grid[1,i]))
+
+	Z = np.asarray(Z)
+	Z = Z.T
+	D = np.asarray(D,dtype='int32')
+	D = D.T
+	A = [[dst[0][0],dst[1][0],dst[2][0]],[dst[0][1],dst[1][1],dst[2][1]],[1,1,1]]
+	coord = np.dot(A,Z)
+	xA = coord[0,:]/coord[2,:]
+	yA = coord[1,:]/coord[2,:]
+
+	C = [xA,yA]
+	C = np.asarray(C)
+
+	return C,D
+
+def interpolate(img,size,pts,det):
+	xi = np.linspace(0, img.shape[1], img.shape[1],endpoint=False)
+	yi = np.linspace(0, img.shape[0], img.shape[0],endpoint=False)
+	#dest = np.zeros((size[0],size[1],3), np.uint8)
+	blue = img[:,:,0]
+	b = interp2d(xi, yi, blue, kind='cubic')
+	green = img[:,:,1]
+	g = interp2d(xi, yi, green, kind='cubic')
+	red = img[:,:,2]
+	r = interp2d(xi, yi, red, kind='cubic')
+	for i,(x,y) in enumerate(pts):
+		bl = b(x,y)
+		gr = g(x,y)
+		re = r(x,y)
+		size[det[i,1],det[i,0]] = (bl,gr,re)
+
+	return size
+
+
+
+def swapFace(d_img,s_img,d_tri,s_tri):
+	L = np.zeros((2,1))
+	D = np.zeros((2,1))
+
+	for i in range(d_tri.shape[0]):
+		z,m = affineBary(s_img,d_tri[i],s_tri[i],d_img.shape)
+		L = np.concatenate((L,z),axis=1)
+		D = np.concatenate((D,m),axis=1)
+	L = np.asarray(L)
+	L = L.T
+	L = L[1:,:]
+	D = np.asarray(D,dtype='int32')
+	D = D.T
+	D = D[1:,:]
+
+	return L,D
+
+def blendFace(hull,dimg,face):
+	mask = np.zeros_like(dimg)
+	cv2.fillPoly(mask, [hull], (255, 255, 255))
+	r = cv2.boundingRect(np.float32([hull]))
+	center = ((r[0]+int(r[2]/2), r[1]+int(r[3]/2)))
+	#cv2.circle(dimg,center,2,(0,255,0),-1)
+	output = cv2.seamlessClone(np.uint8(face), dimg, mask, center, cv2.MIXED_CLONE)
+	return output
+
+def getFaceLandmarks(img,p):
+	detector = dlib.get_frontal_face_detector()
+	predictor = dlib.shape_predictor(p)
+	#img = cv2.imread(fname)
+	#img = imutils.resize(img,width = 320)
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	bbox = detector(gray,0)
+	if (len(bbox)==0):
+		s = np.zeros((68,2))
+		return img,s
+	
+	for (i,bbox) in enumerate(bbox):
+		shape = predictor(gray,bbox)
+		shape = face_utils.shape_to_np(shape)
+		
+		#for (x,y) in shape:
+		#	cv2.circle(img,(x,y),2,(255,0,0),-1)
+
+	return img,shape
 
 
 def main():
